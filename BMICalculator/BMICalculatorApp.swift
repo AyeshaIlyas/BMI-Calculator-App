8157a5f5745a2d40b0f24d34612185fee5b09b82
Ayesha Ilyas <annie16950@gmail.com>
2022-02-22 11:15:31 -0500

    Initial Commit

---
+++ BMICalculator/BMICalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMICalculatorApp.swift
+//  BMICalculator
+//
+//  Created by Ayesha Ilyas on 2/22/22.
+//
+
+import SwiftUI
+
+@main
+struct BMICalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
